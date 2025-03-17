6cbf4eccf71a5b6a04d533c9058745249057086c
kbkn3 <ken0421wabu@gmail.com>
2025-03-17 12:45:41 +0900

    feat: OGデータとTwitterカード情報をメタタグに追加

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,6 +6,17 @@ export function meta({}: Route.MetaArgs) {
   return [
     { title: "kbkn3's portfolio" },
     { name: "description", content: "kbkn3's portfolio" },
+    // OG情報
+    { property: "og:title", content: "kbkn3's portfolio" },
+    { property: "og:description", content: "kbkn3's portfolio" },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://kbkn3.dev/" },
+    { property: "og:image", content: "https://ogp-image-creator.ken0421wabu.workers.dev/portfolio?title=PORTFOLIO" },
+    // Twitter Card
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:title", content: "kbkn3's portfolio" },
+    { name: "twitter:description", content: "kbkn3's portfolio" },
+    { name: "twitter:image", content: "https://ogp-image-creator.ken0421wabu.workers.dev/portfolio?title=PORTFOLIO" },
   ]
 }
 
